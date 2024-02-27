8bee7b3b00bf647ef1f22d2d9b44e38f1acb38e2
HelyssonBN3040 <helyssoncavalcante.com@gmail.com>
2024-02-27 00:19:18 -0300

    att commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,6 @@ import Modal from 'react-bootstrap/Modal';
 
 function Example() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
