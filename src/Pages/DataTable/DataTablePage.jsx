df8dc9f306f93b175304a2bdd69b91789f306101
HelyssonBN3040 <helyssoncavalcante.com.@gmail.com>
2024-02-29 10:34:31 -0300

    alteração de dados

--- src/Pages/DataTable/DataTablePage.jsx
+++ src/Pages/DataTable/DataTablePage.jsx
@@ -68,17 +68,108 @@ const DataTablePage = () => {
 
   ]
 
-  useEffect(() => {
-    const fetchData = async () => {
-      axios.get("https://api-data-testing-445c2ed2517d.herokuapp.com/users")
-        .then(res => {
-          setRecords(res.data);
-          setFilteredRecords(res.data); // Garanta que os dados filtrados também sejam definidos
-        })
-        .catch(err => console.log(err));
-    };
-    fetchData();
-  }, []);
+  const data = [
+    {
+      id: 1,
+      name: "Helysson Cavalcante",
+      cpf: "05265540296"
+    },
+    {
+      id: 2,
+      name: "Mabyle Jeandressa",
+      cpf: "00000000002"
+    },
+    {
+      id: 3,
+      name: "João Victor Almeida",
+      cpf: "00000000003"
+    },
+    {
+      id: 4,
+      name: "Jose Silva Souza",
+      cpf: "00000000004"
+    },
+    {
+      id: 5,
+      name: "Ana Carolina",
+      cpf: "00000000005"
+    },
+    {
+      id: 6,
+      name: "Fernando Oliveira",
+      cpf: "00000000006"
+    },
+    {
+      id: 7,
+      name: "Mariana Santos",
+      cpf: "00000000007"
+    },
+    {
+      id: 8,
+      name: "Lucas Pereira",
+      cpf: "00000000008"
+    },
+    {
+      id: 9,
+      name: "Amanda Silva",
+      cpf: "00000000009"
+    },
+    {
+      id: 10,
+      name: "Rafael Souza",
+      cpf: "00000000010"
+    },
+    {
+      id: 11,
+      name: "Juliana Oliveira",
+      cpf: "00000000011"
+    },
+    {
+      id: 12,
+      name: "Carlos Eduardo",
+      cpf: "00000000012"
+    },
+    {
+      id: 13,
+      name: "Patrícia Santos",
+      cpf: "00000000013"
+    },
+    {
+      id: 14,
+      name: "Bruno Ferreira",
+      cpf: "00000000014"
+    },
+    {
+      id: 15,
+      name: "Camila Lima",
+      cpf: "00000000015"
+    },
+    {
+      id: 16,
+      name: "Marcelo Oliveira",
+      cpf: "00000000016"
+    },
+    {
+      id: 17,
+      name: "Isabela Alves",
+      cpf: "00000000017"
+    },
+    {
+      id: 18,
+      name: "Ricardo Mendes",
+      cpf: "00000000018"
+    },
+    {
+      id: 19,
+      name: "Fernanda Rodrigues",
+      cpf: "00000000019"
+    },
+    {
+      id: 20,
+      name: "Diego Martins",
+      cpf: "00000000020"
+    }
+  ];
 
 
   //função de filtro
@@ -101,7 +192,7 @@ const DataTablePage = () => {
         <h1 className='text-center'>Lista de Pessoas</h1>
         <DataTable
           columns={columns}
-          data={filteredRecords}
+          data={data}
           selectableRowsHighlight
           highlightOnHover
           fixedHeader
